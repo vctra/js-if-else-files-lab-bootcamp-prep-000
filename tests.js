1b38c74f52442de6e5d09d75723b9e3ab1ac3b95
Victoria Hong <victoriaxhong@gmail.com>
2018-05-04 06:31:50 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -1,6 +1,6 @@
 var index = require("./index.js")
 
-if (index.name === "Susan") {
+if (index.name === "Joe") {
     console.log("The name is correct")
 }
 
@@ -10,7 +10,7 @@ else {
 
 
 
-if (index.height === 70) {
+if (index.height === 74) {
     console.log("The height is correct")
 } else {
     console.log("Expected: 70, Received: "+index.height)
