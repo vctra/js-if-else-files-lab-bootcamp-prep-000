fedb3b6b023a026d3b37e7ac382d62275552fefc
Victoria Hong <victoriaxhong@gmail.com>
2018-05-04 06:29:38 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -2,7 +2,9 @@ var index = require("./index.js")
 
 if (index.name === "Susan") {
     console.log("The name is correct")
-} else {
+}
+
+else {
     console.log("Expected: Susan, Received: "+index.name)
 }
 
